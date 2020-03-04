96352931a46599a44dc66f710c9c96da6b40b0d0
Robert Bilsland <RBilsland@users.noreply.github.com>
2020-03-04 21:43:29 +0000

    Initial commit

---
+++ SonicPi.ts
@@ -0,0 +1,104 @@
+/**
+ * Custom blocks
+ */
+//% color=#0fbc11 weight=90 icon="\uf8f1" block="Sonic Pi"
+
+namespace SonicPI {
+
+    let wifi_connected: boolean = false
+    let sonicpiosc_connected: boolean = false
+    let last_upload_successful: boolean = false
+    let userToken_def: string = ""
+    let topic_def: string = ""
+    let recevice_kidiot_text = ""
+    const EVENT_ON_ID = 100
+    const EVENT_ON_Value = 200
+    const EVENT_OFF_ID = 110
+    const EVENT_OFF_Value = 210
+    let toSendStr = ""
+
+    export enum State {
+        //% block="Success"
+        Success,
+        //% block="Fail"
+        Fail
+    }
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 0) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200)
+                serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+                result = true
+                break
+            }
+            if (serial_str.includes("ERROR") || serial_str.includes("FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 5000) {
+                break
+            }
+        }
+        return result
+    }
+
+    /**
+    * Initialize ESP8266 module 
+    */
+    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P12
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_password
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+CWMODE=1") // set to STA mode
+        sendAT("AT+RST", 1000) // reset
+        basic.pause(100)
+    }
+
+    /**
+    * connect to Wifi router
+    */
+    //% block="connect Wifi SSID = %ssid|KEY = %pw"
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw
+    export function connectWifi(ssid: string, pw: string) {
+
+        wifi_connected = false
+		sonicpiosc_connected = false
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        wifi_connected = waitResponse()
+        basic.pause(100)
+    }
+
+    /**
+    * Connect to SonicPiOSC
+    */
+    //% block="connect SonicPi OSC"
+    export function connectSonicPiOSC() {
+        if (wifi_connected) {
+            sonicpiosc_connected = false
+            let text = "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80"
+            sendAT(text, 0) // connect to website server
+            sonicpiosc_connected = waitResponse()
+            basic.pause(100)
+        }
+    }
+}
